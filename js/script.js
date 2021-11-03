26eaacdbdd27ee70f6ecfd4e78a2aeeada720ea5
Mdigennaro <michele.digennaro719@gmail.com>
2021-11-03 12:08:18 +0100

    fine esercizio

--- js/script.js
+++ js/script.js
@@ -1,101 +1,46 @@
-//easy
-//creo gli square dentro il container
 
-const container = document.querySelector('.container');
-
-const listNumbers = [];
-
-const option = document.getElementById('play');
+document.getElementById('play').addEventListener('click', function() {
+  play();
+})
 
 
-if (option === val) {
+function play() {
+
+  const difficolta = parseInt(document.getElementById(`difficolta`).value);
+
+  const grado = [100, 81, 49];
+  const cellNumbers = grado [difficolta - 1];
+  const cellRow = Math.sqrt(cellNumbers);
+
+  document.querySelector('main').innerHTML = "";
+  campoDiGioco();
+
+  function campoDiGioco() {
   
-  for (let i = 0; i < 100; i++) {
-    
-    const squareEasy = createSquare(container);
-  
-  }
-  
-  function createSquare(game) {
+    const campo = document.createElement('div');
+    campo.className= 'container';
 
-    const squareEasy = document.createElement('div');
-    const randomNum = generateUniqueNumber(listNumbers, 1, 100);
-    squareEasy.innerHTML = 
-    `
-    <span>${randomNum}</span>
-    `;
-    squareEasy.className = 'square';
-    game.append(squareEasy);
-    return squareEasy;
-  
-  }
-}
+    for (let i = 1; i <= cellNumbers; i++) {
 
-if (option) {
-  for (let i = 0; i < 100; i++) {
-    
-    const squareEasy = createSquare(container);
-  
-  }
-
-  function createSquare(game) {
-
-    const squareEasy = document.createElement('div');
-    const randomNum = generateUniqueNumber(listNumbers, 1, 64);
-    squareEasy.innerHTML = 
-    `
-    <span>${randomNum}</span>
-    `;
-    squareEasy.className = 'square';
-    game.append(squareEasy);
-    return squareEasy;
-  
-  }
-}
-
-
-/**
- * Generatore di square easy
- * @param {HTMLDivElement} game 
- */
-function createSquare(game) {
-
-  const squareEasy = document.createElement('div');
-  const randomNum = generateUniqueNumber(listNumbers, 1, 100);
-  squareEasy.innerHTML = 
-  `
-  <span>${randomNum}</span>
-  `;
-  squareEasy.className = 'square';
-  game.append(squareEasy);
-  return squareEasy;
-
-}
-
-
-function generateUniqueNumber(list, min, max) {
-
-  let number = null;
-  let valid = false;
-
-  while (!valid) {
-    number = generateRandomNumb(min, max);
-    if(!list.includes(number)){
-      valid = true;
-      list.push(number)
+      const square = document.createElement('div');
+      square.className = 'square';
+      square.innerHTML = 
+      `
+      <span>${i}</span>
+      `;
+      const squareSize = 
+      `
+      calc(100% / ${cellRow})
+      `;
+      square.style.width = squareSize;
+      square.style.height = squareSize;
+      
+      document.querySelector('main').append(campo);
+      campo.append(square);
     }
   }
-  return number;
-
+ 
 }
 
 
-/**
- * Generatore di numeri random
- * @param {number} min 
- * @param {number} max 
- * @returns 
- */
-function generateRandomNumb(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
+
